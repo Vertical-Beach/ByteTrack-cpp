dfddf2a4cc4a7db06608601b4cd2efc1c2f917d3
medalotte <medalotte@gmail.com>
2022-01-22 11:20:33 +0000

    refactor lapjv.(h|cpp)

--- include/ByteTrack/lapjv.h
+++ include/ByteTrack/lapjv.h
@@ -1,70 +1,12 @@
-#ifndef LAPJV_H
-#define LAPJV_H
+#pragma once
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
-#define LARGE 1000000
-
-#if !defined TRUE
-#define TRUE 1
-#endif
-#if !defined FALSE
-#define FALSE 0
-#endif
-
-#define NEW(x, t, n) if ((x = (t *)malloc(sizeof(t) * (n))) == 0) { return -1; }
-#define FREE(x) if (x != 0) { free(x); x = 0; }
-#define SWAP_INDICES(a, b) { byte_track::int_t _temp_index = a; a = b; b = _temp_index; }
-
-#if 0
-#include <assert.h>
-#define ASSERT(cond) assert(cond)
-#define PRINTF(fmt, ...) printf(fmt, ##__VA_ARGS__)
-#define PRINT_COST_ARRAY(a, n) \
-    while (1) { \
-        printf(#a" = ["); \
-        if ((n) > 0) { \
-            printf("%f", (a)[0]); \
-            for (uint_t j = 1; j < n; j++) { \
-                printf(", %f", (a)[j]); \
-            } \
-        } \
-        printf("]\n"); \
-        break; \
-    }
-#define PRINT_INDEX_ARRAY(a, n) \
-    while (1) { \
-        printf(#a" = ["); \
-        if ((n) > 0) { \
-            printf("%d", (a)[0]); \
-            for (uint_t j = 1; j < n; j++) { \
-                printf(", %d", (a)[j]); \
-            } \
-        } \
-        printf("]\n"); \
-        break; \
-    }
-#else
-#define ASSERT(cond)
-#define PRINTF(fmt, ...)
-#define PRINT_COST_ARRAY(a, n)
-#define PRINT_INDEX_ARRAY(a, n)
-#endif
+#include <iostream>
 
 namespace byte_track
 {
-
-typedef signed int int_t;
-typedef unsigned int uint_t;
-typedef double cost_t;
-typedef char boolean;
-typedef enum fp_t { FP_1 = 1, FP_2 = 2, FP_DYNAMIC = 3 } fp_t;
-
-extern int_t lapjv_internal(
-    const uint_t n, cost_t *cost[],
-    int_t *x, int_t *y);
+int lapjv_internal(const size_t n, double *cost[], int *x, int *y);
 }
-
-#endif // LAPJV_H
--- src/lapjv.cpp
+++ src/lapjv.cpp
@@ -1,102 +1,106 @@
 #include <ByteTrack/lapjv.h>
 
-/** Column-reduction and reduction transfer for a dense cost matrix.
- */
-byte_track::int_t _ccrrt_dense(const byte_track::uint_t n, byte_track::cost_t *cost[],
-    byte_track::int_t *free_rows, byte_track::int_t *x, byte_track::int_t *y, byte_track::cost_t *v)
-{
-    byte_track::int_t n_free_rows;
-    byte_track::boolean *unique;
+#define LAPJV_CPP_NEW(x, t, n) if ((x = (t *)malloc(sizeof(t) * (n))) == 0) { return -1; }
+#define LAPJV_CPP_FREE(x) if (x != 0) { free(x); x = 0; }
+#define LAPJV_CPP_SWAP_INDICES(a, b) { int _temp_index = a; a = b; b = _temp_index; }
 
-    for (byte_track::uint_t i = 0; i < n; i++) {
+namespace
+{
+constexpr size_t LARGE = 1000000;
+
+enum class fp_t {
+    FP_1 = 1,
+    FP_2 = 2,
+    FP_DYNAMIC = 3,
+};
+
+/** Column-reduction and reduction transfer for a dense cost matrix.
+*/
+int _ccrrt_dense(const size_t n, double *cost[],
+    int *free_rows, int *x, int *y, double *v)
+{
+    int n_free_rows;
+    bool *unique;
+
+    for (size_t i = 0; i < n; i++) {
         x[i] = -1;
         v[i] = LARGE;
         y[i] = 0;
     }
-    for (byte_track::uint_t i = 0; i < n; i++) {
-        for (byte_track::uint_t j = 0; j < n; j++) {
-            const byte_track::cost_t c = cost[i][j];
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
+            const double c = cost[i][j];
             if (c < v[j]) {
                 v[j] = c;
                 y[j] = i;
             }
-            PRINTF("i=%d, j=%d, c[i,j]=%f, v[j]=%f y[j]=%d\n", i, j, c, v[j], y[j]);
         }
     }
-    PRINT_COST_ARRAY(v, n);
-    PRINT_INDEX_ARRAY(y, n);
-    NEW(unique, byte_track::boolean, n);
-    memset(unique, TRUE, n);
+    LAPJV_CPP_NEW(unique, bool, n);
+    memset(unique, true, n);
     {
-        byte_track::int_t j = n;
+        int j = n;
         do {
             j--;
-            const byte_track::int_t i = y[j];
+            const int i = y[j];
             if (x[i] < 0) {
                 x[i] = j;
             }
             else {
-                unique[i] = FALSE;
+                unique[i] = false;
                 y[j] = -1;
             }
         } while (j > 0);
     }
     n_free_rows = 0;
-    for (byte_track::uint_t i = 0; i < n; i++) {
+    for (size_t i = 0; i < n; i++) {
         if (x[i] < 0) {
             free_rows[n_free_rows++] = i;
         }
         else if (unique[i]) {
-            const byte_track::int_t j = x[i];
-            byte_track::cost_t min = LARGE;
-            for (byte_track::uint_t j2 = 0; j2 < n; j2++) {
-                if (j2 == (byte_track::uint_t)j) {
+            const int j = x[i];
+            double min = LARGE;
+            for (size_t j2 = 0; j2 < n; j2++) {
+                if (j2 == (size_t)j) {
                     continue;
                 }
-                const byte_track::cost_t c = cost[i][j2] - v[j2];
+                const double c = cost[i][j2] - v[j2];
                 if (c < min) {
                     min = c;
                 }
             }
-            PRINTF("v[%d] = %f - %f\n", j, v[j], min);
             v[j] -= min;
         }
     }
-    FREE(unique);
+    LAPJV_CPP_FREE(unique);
     return n_free_rows;
 }
 
 
 /** Augmenting row reduction for a dense cost matrix.
  */
-byte_track::int_t _carr_dense(
-    const byte_track::uint_t n, byte_track::cost_t *cost[],
-    const byte_track::uint_t n_free_rows,
-    byte_track::int_t *free_rows, byte_track::int_t *x, byte_track::int_t *y, byte_track::cost_t *v)
+int _carr_dense(
+    const size_t n, double *cost[],
+    const size_t n_free_rows,
+    int *free_rows, int *x, int *y, double *v)
 {
-    byte_track::uint_t current = 0;
-    byte_track::int_t new_free_rows = 0;
-    byte_track::uint_t rr_cnt = 0;
-    PRINT_INDEX_ARRAY(x, n);
-    PRINT_INDEX_ARRAY(y, n);
-    PRINT_COST_ARRAY(v, n);
-    PRINT_INDEX_ARRAY(free_rows, n_free_rows);
+    size_t current = 0;
+    int new_free_rows = 0;
+    size_t rr_cnt = 0;
     while (current < n_free_rows) {
-        byte_track::int_t i0;
-        byte_track::int_t j1, j2;
-        byte_track::cost_t v1, v2, v1_new;
-        byte_track::boolean v1_lowers;
+        int i0;
+        int j1, j2;
+        double v1, v2, v1_new;
+        bool v1_lowers;
 
         rr_cnt++;
-        PRINTF("current = %d rr_cnt = %d\n", current, rr_cnt);
-        const byte_track::int_t free_i = free_rows[current++];
+        const int free_i = free_rows[current++];
         j1 = 0;
         v1 = cost[free_i][0] - v[0];
         j2 = -1;
         v2 = LARGE;
-        for (byte_track::uint_t j = 1; j < n; j++) {
-            PRINTF("%d = %f %d = %f\n", j1, v1, j2, v2);
-            const byte_track::cost_t c = cost[free_i][j] - v[j];
+        for (size_t j = 1; j < n; j++) {
+            const double c = cost[free_i][j] - v[j];
             if (c < v2) {
                 if (c >= v1) {
                     v2 = c;
@@ -113,7 +117,6 @@ byte_track::int_t _carr_dense(
         i0 = y[j1];
         v1_new = v[j1] - (v2 - v1);
         v1_lowers = v1_new < v[j1];
-        PRINTF("%d %d 1=%d,%f 2=%d,%f v1'=%f(%d,%g) \n", free_i, i0, j1, v1, j2, v2, v1_new, v1_lowers, v[j1] - v1_new);
         if (rr_cnt < current * n) {
             if (v1_lowers) {
                 v[j1] = v1_new;
@@ -132,7 +135,6 @@ byte_track::int_t _carr_dense(
             }
         }
         else {
-            PRINTF("rr_cnt=%d >= %d (current=%d * n=%d)\n", rr_cnt, current * n, current, n);
             if (i0 >= 0) {
                 free_rows[new_free_rows++] = i0;
             }
@@ -146,12 +148,12 @@ byte_track::int_t _carr_dense(
 
 /** Find columns with minimum d[j] and put them on the SCAN list.
  */
-byte_track::uint_t _find_dense(const byte_track::uint_t n, byte_track::uint_t lo, byte_track::cost_t *d, byte_track::int_t *cols, byte_track::int_t *y)
+size_t _find_dense(const size_t n, size_t lo, double *d, int *cols, int *y)
 {
-    byte_track::uint_t hi = lo + 1;
-    byte_track::cost_t mind = d[cols[lo]];
-    for (byte_track::uint_t k = hi; k < n; k++) {
-        byte_track::int_t j = cols[k];
+    size_t hi = lo + 1;
+    double mind = d[cols[lo]];
+    for (size_t k = hi; k < n; k++) {
+        int j = cols[k];
         if (d[j] <= mind) {
             if (d[j] < mind) {
                 hi = lo;
@@ -167,23 +169,22 @@ byte_track::uint_t _find_dense(const byte_track::uint_t n, byte_track::uint_t lo
 
 // Scan all columns in TODO starting from arbitrary column in SCAN
 // and try to decrease d of the TODO columns using the SCAN column.
-byte_track::int_t _scan_dense(const byte_track::uint_t n, byte_track::cost_t *cost[],
-    byte_track::uint_t *plo, byte_track::uint_t*phi,
-    byte_track::cost_t *d, byte_track::int_t *cols, byte_track::int_t *pred,
-    byte_track::int_t *y, byte_track::cost_t *v)
+int _scan_dense(const size_t n, double *cost[],
+    size_t *plo, size_t*phi,
+    double *d, int *cols, int *pred,
+    int *y, double *v)
 {
-    byte_track::uint_t lo = *plo;
-    byte_track::uint_t hi = *phi;
-    byte_track::cost_t h, cred_ij;
+    size_t lo = *plo;
+    size_t hi = *phi;
+    double h, cred_ij;
 
     while (lo != hi) {
-        byte_track::int_t j = cols[lo++];
-        const byte_track::int_t i = y[j];
-        const byte_track::cost_t mind = d[j];
+        int j = cols[lo++];
+        const int i = y[j];
+        const double mind = d[j];
         h = cost[i][j] - v[j] - mind;
-        PRINTF("i=%d j=%d h=%f\n", i, j, h);
         // For all columns in TODO
-        for (byte_track::uint_t k = hi; k < n; k++) {
+        for (size_t k = hi; k < n; k++) {
             j = cols[k];
             cred_ij = cost[i][j] - v[j] - h;
             if (cred_ij < d[j]) {
@@ -211,64 +212,55 @@ byte_track::int_t _scan_dense(const byte_track::uint_t n, byte_track::cost_t *co
  *
  * \return The closest free column index.
  */
-byte_track::int_t find_path_dense(
-    const byte_track::uint_t n, byte_track::cost_t *cost[],
-    const byte_track::int_t start_i,
-    byte_track::int_t *y, byte_track::cost_t *v,
-    byte_track::int_t *pred)
+int find_path_dense(
+    const size_t n, double *cost[],
+    const int start_i,
+    int *y, double *v,
+    int *pred)
 {
-    byte_track::uint_t lo = 0, hi = 0;
-    byte_track::int_t final_j = -1;
-    byte_track::uint_t n_ready = 0;
-    byte_track::int_t *cols;
-    byte_track::cost_t *d;
+    size_t lo = 0, hi = 0;
+    int final_j = -1;
+    size_t n_ready = 0;
+    int *cols;
+    double *d;
 
-    NEW(cols, byte_track::int_t, n);
-    NEW(d, byte_track::cost_t, n);
+    LAPJV_CPP_NEW(cols, int, n);
+    LAPJV_CPP_NEW(d, double, n);
 
-    for (byte_track::uint_t i = 0; i < n; i++) {
+    for (size_t i = 0; i < n; i++) {
         cols[i] = i;
         pred[i] = start_i;
         d[i] = cost[start_i][i] - v[i];
     }
-    PRINT_COST_ARRAY(d, n);
+
     while (final_j == -1) {
         // No columns left on the SCAN list.
         if (lo == hi) {
-            PRINTF("%d..%d -> find\n", lo, hi);
             n_ready = lo;
             hi = _find_dense(n, lo, d, cols, y);
-            PRINTF("check %d..%d\n", lo, hi);
-            PRINT_INDEX_ARRAY(cols, n);
-            for (byte_track::uint_t k = lo; k < hi; k++) {
-                const byte_track::int_t j = cols[k];
+            for (size_t k = lo; k < hi; k++) {
+                const int j = cols[k];
                 if (y[j] < 0) {
                     final_j = j;
                 }
             }
         }
         if (final_j == -1) {
-            PRINTF("%d..%d -> scan\n", lo, hi);
             final_j = _scan_dense(
                 n, cost, &lo, &hi, d, cols, pred, y, v);
-            PRINT_COST_ARRAY(d, n);
-            PRINT_INDEX_ARRAY(cols, n);
-            PRINT_INDEX_ARRAY(pred, n);
         }
     }
 
-    PRINTF("found final_j=%d\n", final_j);
-    PRINT_INDEX_ARRAY(cols, n);
     {
-        const byte_track::cost_t mind = d[cols[lo]];
-        for (byte_track::uint_t k = 0; k < n_ready; k++) {
-            const byte_track::int_t j = cols[k];
+        const double mind = d[cols[lo]];
+        for (size_t k = 0; k < n_ready; k++) {
+            const int j = cols[k];
             v[j] += d[j] - mind;
         }
     }
 
-    FREE(cols);
-    FREE(d);
+    LAPJV_CPP_FREE(cols);
+    LAPJV_CPP_FREE(d);
 
     return final_j;
 }
@@ -276,54 +268,54 @@ byte_track::int_t find_path_dense(
 
 /** Augment for a dense cost matrix.
  */
-byte_track::int_t _ca_dense(
-    const byte_track::uint_t n, byte_track::cost_t *cost[],
-    const byte_track::uint_t n_free_rows,
-    byte_track::int_t *free_rows, byte_track::int_t *x, byte_track::int_t *y, byte_track::cost_t *v)
+int _ca_dense(
+    const size_t n, double *cost[],
+    const size_t n_free_rows,
+    int *free_rows, int *x, int *y, double *v)
 {
-    byte_track::int_t *pred;
+    int *pred;
 
-    NEW(pred, byte_track::int_t, n);
+    LAPJV_CPP_NEW(pred, int, n);
 
-    for (byte_track::int_t *pfree_i = free_rows; pfree_i < free_rows + n_free_rows; pfree_i++) {
-        byte_track::int_t i = -1, j;
-        byte_track::uint_t k = 0;
+    for (int *pfree_i = free_rows; pfree_i < free_rows + n_free_rows; pfree_i++) {
+        int i = -1, j;
+        size_t k = 0;
 
-        PRINTF("looking at free_i=%d\n", *pfree_i);
         j = find_path_dense(n, cost, *pfree_i, y, v, pred);
-        ASSERT(j >= 0);
-        ASSERT(j < n);
+        if (j < 0)
+        {
+            throw std::runtime_error("Error occured in _ca_dense(): j < 0");
+        }
+        if (j >= static_cast<int>(n))
+        {
+            throw std::runtime_error("Error occured in _ca_dense(): j >= n");
+        }
         while (i != *pfree_i) {
-            PRINTF("augment %d\n", j);
-            PRINT_INDEX_ARRAY(pred, n);
             i = pred[j];
-            PRINTF("y[%d]=%d -> %d\n", j, y[j], i);
             y[j] = i;
-            PRINT_INDEX_ARRAY(x, n);
-            SWAP_INDICES(j, x[i]);
+            LAPJV_CPP_SWAP_INDICES(j, x[i]);
             k++;
             if (k >= n) {
-                ASSERT(FALSE);
+                throw std::runtime_error("Error occured in _ca_dense(): k >= n");
             }
         }
     }
-    FREE(pred);
+    LAPJV_CPP_FREE(pred);
     return 0;
 }
+}
 
-
-/** Solve dense sparse LAP.
- */
+/** Solve dense sparse LAP. */
 int byte_track::lapjv_internal(
-    const byte_track::uint_t n, byte_track::cost_t *cost[],
-    byte_track::int_t *x, byte_track::int_t *y)
+    const size_t n, double *cost[],
+    int *x, int *y)
 {
     int ret;
-    byte_track::int_t *free_rows;
-    byte_track::cost_t *v;
+    int *free_rows;
+    double *v;
 
-    NEW(free_rows, byte_track::int_t, n);
-    NEW(v, byte_track::cost_t, n);
+    LAPJV_CPP_NEW(free_rows, int, n);
+    LAPJV_CPP_NEW(v, double, n);
     ret = _ccrrt_dense(n, cost, free_rows, x, y, v);
     int i = 0;
     while (ret > 0 && i < 2) {
@@ -333,7 +325,11 @@ int byte_track::lapjv_internal(
     if (ret > 0) {
         ret = _ca_dense(n, cost, ret, free_rows, x, y, v);
     }
-    FREE(v);
-    FREE(free_rows);
+    LAPJV_CPP_FREE(v);
+    LAPJV_CPP_FREE(free_rows);
     return ret;
 }
+
+#undef LAPJV_CPP_NEW
+#undef LAPJV_CPP_FREE
+#undef LAPJV_CPP_SWAP_INDICES
